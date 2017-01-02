03b53761018eb2c86fddbde7210781dd80c1018a
Matt Deuschle <mdeuschle@gmail.com>
2017-01-02 12:42:12 -0800

    request permission

--- UserNotification/ViewController.swift
+++ UserNotification/ViewController.swift
@@ -7,19 +7,23 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .badge, .sound], completionHandler: { (response, error) in
+            if response {
+                print("Permission Granted")
+            } else {
+                if let err = error {
+                    print(err.localizedDescription)
+                }
+            }
+        })
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
