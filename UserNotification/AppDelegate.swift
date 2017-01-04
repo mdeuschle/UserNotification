af608d25625cbec3e6b413d53b3e3336c8b0dac9
Matt Deuschle <mdeuschle@gmail.com>
2017-01-03 18:34:22 -0800

    notifications in the foreground

--- UserNotification/AppDelegate.swift
+++ UserNotification/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        UNUserNotificationCenter.current().delegate = self
         return true
     }
 
@@ -40,7 +43,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+}
 
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+
+    // show notifications when app is in foregroud
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler(.alert)
+    }
 
 }
+
+
+
 
